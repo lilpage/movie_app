21622da61e17e8ccf07e9b53cbcae165ea31f878
Lil <lil@Lils-MacBook-Air.local>
2021-03-13 19:03:22 -0600

    Uploads 2.4 hw

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,13 @@
+class Api::MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    render "index.json.jb"
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Actor.destroy_all
+Movie.destroy_all
 
 10.times do
   Actor.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
 end
+
+Movie.create(title: "Romeo+Juliet", year: 1996, plot: "Two households, both alike in dignity, in fair Verona where we set our scene. From ancient grudge break new mutiny, where civil blood makes civil hands unclean. From forth the fatal loins of these two foes, a pair of star-cross'd lovers take their life: whose misadventures piteous overthrows, do with their death bury their parents' strife.")
