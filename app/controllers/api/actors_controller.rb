eb5bfe346f5085c03642e4df3bd71cb6f01b05b5
Lil <lil@Lils-MacBook-Air.local>
2021-03-14 12:13:33 -0500

    Updates actor model with age and gender. Fixes update and create methods for actor

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -9,6 +9,8 @@ class Api::ActorsController < ApplicationController
     @actor = Actor.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
+      age: params[:age],
+      gender: params[:gender],
       known_for: params[:known_for]
     )
     @actor.save
@@ -18,6 +20,16 @@ class Api::ActorsController < ApplicationController
   def show
     @actor = Actor.find_by(id: params[:id])
     render "show.json.jb"
+  end
+
+  def update
+    @actor = Actor.find_by(id: params[:id])
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.age = params[:age] || @actor.age
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.save
   end
 
 end
